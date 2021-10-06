3a134e0ca44f66f50af1f9c7b5d28b64f17658f5
zataara <bryantmac@gmail.com>
2021-10-06 00:01:13 -0700

    added register classmethod

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 /** User class for message.ly */
 
+const { DB_URI } = require("../config");
+const bcrypt = require("bcrypt");
+const config = require("./config")
+
+
 
 
 /** User of the site. */
@@ -10,11 +15,38 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    const hashedPassword = await bcrypt.hash(
+      password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (
+        username,
+        password,
+        first_name,
+        last_name,
+        phone,
+        join_at,
+        last_login_at
+      )
+      VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)`,
+      [username, hashedPassword, first_name, last_name, phone]
+    );
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+      FROM users
+      WHERE user = $1`,
+      [username]
+    )
+    const user = result.rows[0];
+    
+  }
+
 
   /** Update last_login_at for user */
 

c3fa09617f26cc01ca773e650aac3a2f9de6e5a2
zataara <bryantmac@gmail.com>
2021-10-06 20:53:13 -0700

    added route for reading a message

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const { nextTick } = require("process");
-const router = express.Router();
-const db = require("../db");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+const Message = require("../models/message");
+const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -16,6 +16,19 @@ const jwt = require("jsonwebtoken");
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureCorrectUser, async function(req, res, next) {
+    try{
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id)
+
+        if (msg.to_user.username !== username && msg.from_user.username !== username) {
+            throw new ExpressError("Cannot read this message", 401);
+        }
+        return res.json({message: msg})
+    } catch(e) {
+        return next(e)
+    }
+})
 
 
 /** POST / - post message.
@@ -24,6 +37,18 @@ const jwt = require("jsonwebtoken");
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/', ensureCorrectUser, async function(req, res, next) {
+    try {
+        const msg = await Message.create({
+            from_username: req.user.username,
+            to_username: req.body.to_username,
+            body: req.body.body
+        });
+        return res.json({message: msg})
+    } catch(e) {
+        return next(e)
+    }
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -33,7 +58,20 @@ const jwt = require("jsonwebtoken");
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post('/:id/read', ensureCorrectUser, async function(req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id)
 
+        if (msg.to_user.username !== username) {
+            throw new ExpressError("Cannot set this message to read", 401);
+        }
+        let readMessage = Message.markRead(id);
+        return res.json({readMessage})
+    } catch(e) {
+        return next(e)
+    }
+})
 
 
 module.exports = router;

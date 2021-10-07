35b8304867590782adf6585ab1b4e264695dfba6
zataara <bryantmac@gmail.com>
2021-10-06 21:17:25 -0700

    changing BCRYPT_WORK_FACTOR to variate depending on test environment

--- config.js
+++ config.js
@@ -10,7 +10,9 @@ const DB_URI = (process.env.NODE_ENV === "test")
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
-const BCRYPT_WORK_FACTOR = 12;
+const BCRYPT_WORK_FACTOR = (process.env.NODE_ENV === "test")
+? 1
+: 12;
 
 
 module.exports = {
